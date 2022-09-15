6d3925562d1df24be379571ee39efec5a3c1f49b
Larissa Wagnerberger <larissaw@kth.se>
2022-09-15 18:11:35 +0200

    set up basic router for API

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { getAPI} from "./webAPI"
 export default function App() {
+  const [fetchedData, setFetchedData] = React.useState("");
+  React.useEffect(()=>{
+getAPI().then(data => {setFetchedData(data)
+
+})
+  }, [])
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <div>{`${fetchedData}`}</div>
       <StatusBar style="auto" />
     </View>
   );
--- server/src/start.ts
+++ server/src/start.ts
@@ -1,20 +1,29 @@
 import express, {Application} from "express";
 import path from "path";
-import http from "http";
+import https from "https";
+import fs from 'fs';
+import cors from "cors";
+const api = require('./api')
 const app =  express();
-
 const port = process.env.PORT || 8080;
-const router = express.Router;
+const options = { 
+    key: fs.readFileSync("./cert/localhost-key.pem"),
+    cert: fs.readFileSync("./cert/localhost.pem"),
+
+}
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../../dist")));
-
+app.use(cors({credentials:true, origin: "http://localhost:19006"}))
 app.get("/", (req: express.Request, res: express.Response) => {
     const htmlFile = path.join(__dirname, "../../dist/index.html")
     res.status(200).send(htmlFile);
-})
+});
+app.use('/api', api)
 
-const server = http.createServer( app);
+
+
+const server = https.createServer( options,app);
 server.listen(port, () =>{
     console.log("Server is listening on port:"+port);
 })
