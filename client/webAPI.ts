572c44f9b4c10deff15cba07ede68e4ef8289a19
Larissa Wagnerberger <larissaw@kth.se>
2022-09-15 19:14:23 +0200

    get randomMeal fromAPI

--- client/webAPI.ts
+++ client/webAPI.ts
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export const getAPI = () => axios.get("https://localhost:8080/api/get", {withCredentials:true }).then(({data}:{data: {someData:string}}) =>data.someData)
+export const getAPI = () => axios.get("https://localhost:8080/api/get/randomMeal", {withCredentials:true })
--- server/src/api.ts
+++ server/src/api.ts
@@ -1,14 +1,17 @@
+import axios from "axios";
+
 const express = require('express')
 const router = express.Router()
 
+axios.defaults.baseURL = 'https://www.themealdb.com/api/json/v1/1/';
 // middleware that is specific to this router
 router.use((req, res, next) => {
   console.log('Time: ', Date.now())
   next()
 })
 // define the home page route
-router.get('/get', (req, res) => {
-  res.send({someData: "yeah!"})
+router.get('/get/randomMeal', (req, res) => {
+ axios.get("/random.php").then(({data}:{data: {meals:[{}]}}) =>res.send(data.meals[0]))
 })
 
 
