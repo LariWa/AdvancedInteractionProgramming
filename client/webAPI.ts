6f1948163386648d6d847a6648403edc91dc2818
Larissa Wagnerberger <larissaw@kth.se>
2022-09-17 19:18:13 +0200

    add api calls

--- client/webAPI.ts
+++ client/webAPI.ts
@@ -1,3 +1,12 @@
 import axios from "axios";
 
-export const getAPI = () => axios.get("https://localhost:8080/api/get/randomMeal", {withCredentials:true })
+export const getAPI = () =>
+  axios.post(
+    "https://localhost:8080/api/post/mealDetails",
+    { id: 52772 },
+    {
+      withCredentials: true,
+    }
+  );
+
+//export const getAPI = () => axios.post("https://localhost:8080/api/post/filterMeals", {c: "Seafood", a:"Canadian", i:"Salmon"}, {withCredentials:true})
--- server/src/api.ts
+++ server/src/api.ts
@@ -1,18 +1,51 @@
 import axios from "axios";
 
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-axios.defaults.baseURL = 'https://www.themealdb.com/api/json/v1/1/';
+axios.defaults.baseURL = "https://www.themealdb.com/api/json/v1/1/";
 // middleware that is specific to this router
 router.use((req, res, next) => {
-  console.log('Time: ', Date.now())
-  next()
-})
+  console.log("Time: ", Date.now());
+  next();
+});
 // define the home page route
-router.get('/get/randomMeal', (req, res) => {
- axios.get("/random.php").then(({data}:{data: {meals:[{}]}}) =>res.send(data.meals[0]))
-})
+//gets random meal
+router.get("/get/randomMeal", (req, res) => {
+  axios
+    .get("/random.php")
+    .then(( response  => res.send(getMeal);
+});
 
+//get meal details
+router.post("/post/mealDetails", (req, res) => {
+  console.log(req.body);
+  axios
+    .get("/lookup.php?i=" + req.body.id)
+    .then(getMeal)
+    .then((data) => res.send(data));
+});
 
-module.exports = router
+//get all meal categories
+router.get("/get/categories", (req, res) => {
+  axios
+    .get("/categories.php")
+    .then((response) => res.send(response.data.categories));
+});
+
+//gets meals filtered by category, area, ingredientss
+router.post("/post/filterMeals", (req, res) => {
+  axios
+    .get("filter.php?" + new URLSearchParams(req.body))
+    .then(getMeals)
+    .then((data) => res.send(data));
+});
+
+function getMeal(response) {
+  console.log(response.data.meals[0]);
+  return response.data.meals[0];
+}
+function getMeals(response) {
+  return response.data.meals;
+}
+module.exports = router;
