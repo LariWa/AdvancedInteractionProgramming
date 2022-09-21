02b1a2b2100ee531154c6cdb6323773d390a3d4c
anna <anna>
2022-09-21 15:20:32 +0200

    redux setup

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,15 +1,68 @@
-const express = require('express')
-const router = express.Router()
+import axios from "axios";
+import { key, version } from "./mealAPI";
+var _ = require("lodash");
+const express = require("express");
+const router = express.Router();
 
+axios.defaults.baseURL =
+  "https://www.themealdb.com/api/json/" + version + "/" + key + "//";
 // middleware that is specific to this router
 router.use((req, res, next) => {
-  console.log('Time: ', Date.now())
-  next()
-})
+  console.log("Time: ", Date.now());
+  next();
+});
 // define the home page route
-router.get('/get', (req, res) => {
-  res.send({someData: "yeah!"})
-})
+//gets random meal
+router.get("/get/randomMeal", (req, res) => {
+  console.log(1)
+  axios
+    .get("/random.php")
+    .then(getMeal)
+    .then((meal) => res.send(meal));
+});
 
+//get meal details
+router.post("/post/mealDetails", (req, res) => {
+  console.log(req.body);
+  axios
+    .get("/lookup.php?i=" + req.body.id)
+    .then(getMeal)
+    .then((data) => res.send(data));
+});
 
-module.exports = router
+//get all meal categories
+router.get("/get/categories", (req, res) => {
+  axios
+    .get("/categories.php")
+    .then((response) => res.send(response.data.categories));
+});
+
+//gets meals filtered by category, area, ingredients
+router.post("/post/filterMeals", (req, res) => {
+  let filterPromises = [];
+  if (req.body.category)
+    filterPromises.push(axios.get("filter.php?c=" + req.body.category));
+  if (req.body.area)
+    filterPromises.push(axios.get("filter.php?a=" + req.body.area));
+  if (req.body.ingredients)
+    filterPromises.push(
+      axios.get("filter.php?i=" + req.body.ingredients.toString())
+    );
+  if (req.body.query)
+    filterPromises.push(axios.get("search.php?s=" + req.body.query));
+
+  axios.all(filterPromises).then((response) => {
+    console.log(_.intersectionBy(...response.map(getMeals), "idMeal"));
+    res.send(_.intersectionBy(...response.map(getMeals), "idMeal"));
+  });
+});
+
+function getMeal(response) {
+  console.log(response.data.meals[0]);
+  return response.data.meals[0];
+}
+function getMeals(response) {
+  return response.data.meals;
+}
+
+module.exports = router;
---
+++ server/src/mealAPI.ts
@@ -0,0 +1,3 @@
+const key = "9973533";
+const version = "v2";
+export { key, version };
