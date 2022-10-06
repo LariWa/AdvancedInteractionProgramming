4ca0607365d9c33a22fdb8515bdd9eca031ed06a
anna <anna>
2022-10-06 19:12:26 +0200

    welcome page update

--- client/views/WelcomeView.tsx
+++ client/views/WelcomeView.tsx
@@ -1,50 +1,69 @@
-import { Button, StyleSheet, TouchableOpacity } from 'react-native';
-
-import EditViewInfo from '../components/EditViewInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-export default function WelcomeView({ navigation }: RootTabScreenProps<'TabOne'>) {
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome to the Meal App</Text>
-      <Text style={styles.title}>Welcome to the Meal App</Text>
-      <Text style={styles.title}>Welcome to the Meal App</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('TabTwo')} >
-        <Text>Get started</Text>
-      </TouchableOpacity>
-      
-    </View>
-  );
-}
+import React from 'react'
+import { Image, StyleSheet } from "react-native";
+import { Flex, Text, Button } from "@react-native-material/core";
+const previewImage = require("../pictures/previewImage.png");
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'lightblue',
-  },
-  title: {
-    textAlign: 'center',
-    fontSize: 40,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-  button: {
-    fontWeight: "bold",
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 20,
-    marginVertical: 4
-  },
-});
+    containerOne: {
+        backgroundColor: "#FFFFFF"
+    },
+    containerTwo: { 
+        backgroundColor: "#F4FFDC", 
+        borderTopLeftRadius: 0,
+        borderTopRightRadius: 0,
+    },
+    containerTwo_centered: { 
+        justifyContent:"center", 
+        alignContent:"center"
+    },
+    containerTwo_centered_h5: {
+        marginRight:"auto", 
+        marginLeft:"auto", 
+        fontWeight:"700"
+    },
+    containerTwo_centered_h6: {
+        marginRight:"auto", 
+        marginLeft:"auto", 
+        fontWeight:"400"
+    },
+    button: {
+        backgroundColor: 'white', 
+        width:"200px", 
+        height:"60px", 
+        marginRight:"auto", 
+        marginLeft:"auto", 
+        paddingTop: "10px", 
+        paddingBottom:"10px", 
+        borderRadius: 10,
+    },
+    containerOne_image: {
+        width: "100%",
+        height: "100%",
+        top: "0",
+        left: "0",
+        position: "absolute"
+    }
+   
+   });
+
+export default function WelcomeView(props: any) {
+    const handeStartACB = () => {
+        props.getStarted()
+    }
+    return (
+    <Flex fill>
+        <Flex fill style={styles.containerOne}>
+            <Image style={styles.containerOne_image} source={require('../pictures/previewImage.png')} ></Image>
+        </Flex>
+        <Flex fill style={styles.containerTwo}>
+            <Flex fill style={styles.containerTwo_centered}>
+                <Text variant="h5" style={styles.containerTwo_centered_h5} >Welcome</Text>
+                <Text variant="h6" style={styles.containerTwo_centered_h6} >Welcome text Welcome text</Text>
+            </Flex>
+            <Flex fill style={styles.containerTwo_centered}>
+            <Button title="Get Started" style={styles.button} color="red" onPress={handeStartACB}/>
+            </Flex>
+        </Flex>
+    </Flex>
+  )
+}
