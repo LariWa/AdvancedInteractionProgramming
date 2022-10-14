ef6451f45bd5f66896bfc814ca0b4245ba588ac0
Larissa Wagnerberger <larissa.wagnerberger@gmail.com>
2022-10-14 15:33:25 +0200

    Delete favoritesPresenter.tsx

--- client/presenters/favoritesPresenter.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import FavoritesView from '../views/favoritesView'
-
-export default function FavoritesPresenter() {
-  return (
-    <FavoritesView></FavoritesView>
-  )
-}
