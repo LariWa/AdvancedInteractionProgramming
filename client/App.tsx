c2088988301a79586693941b85ed8ca77043bd04
Borja Javierre <borja.general@gmail.com>
2022-10-21 13:36:43 +0200

    changing color scheme appearance

--- client/App.tsx
+++ client/App.tsx
@@ -4,12 +4,15 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
+import { Appearance } from 'react-native'; //this is used to get a dark mode for the appearance
 import Navigation from "./navigation";
 import SnackbarPresenter from "./presenters/snackbarPresenter";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  //const colorScheme = useColorScheme();
+  const colorScheme = Appearance.getColorScheme();
+
 
   if (!isLoadingComplete) {
     return null;
