d003f0220ef8b7c39c6c14f295d10c3afbdb8ffc
Larissa Wagnerberger <larissa.wagnerberger@gmail.com>
2022-10-23 20:31:26 +0200

    Update recipePresenter.tsx

--- client/presenters/recipePresenter.tsx
+++ client/presenters/recipePresenter.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import RecipeView from "../views/recipeView";
 import useColorScheme from "../hooks/useColorScheme";
-import { useSelector, useDispatch } from "react-redux";
-import { addIngr } from "../redux/actions/ingredientsActions";
 
 export default function RecipePresenter({ route }: any, props: any) {
-  const dispatch = useDispatch<any>();
   const { recipe } = route.params;
   const colorScheme = useColorScheme();
 
-  function addToListACB(ingredient: any){
-    dispatch(addIngr(ingredient));
-  }
-  return <RecipeView recipe={recipe} onAddToList = {addToListACB} colorScheme={colorScheme} />;
+  return <RecipeView recipe={recipe} colorScheme={colorScheme} />;
 }
