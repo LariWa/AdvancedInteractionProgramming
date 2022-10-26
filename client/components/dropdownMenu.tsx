265c8e6185c07f6570b2fd36b538e5818884339f
Larissa Wagnerberger <larissa.wagnerberger@gmail.com>
2022-10-26 09:35:49 +0200

    fix dropdown - clearer selection

--- client/components/dropdownMenu.tsx
+++ client/components/dropdownMenu.tsx
@@ -2,25 +2,26 @@ import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import { MultiSelect } from "react-native-element-dropdown";
 
-const styles = (props: any) => StyleSheet.create({
-  dropdown: {
-    backgroundColor: props.colorScheme == "dark" ? "#2F2F2F" : "#FDFBF7",
-    fontColor: props.colorScheme == "dark" ? "white" : "#2F2F2F",
-    borderRadius: 10,
-    padding: 10,
-    width: 150,
-    marginRight: 10,
-  },
-  placeholderStyle: {
-    fontSize: 16,
-    color: props.colorScheme == "dark" ? "white" : "#2F2F2F",
-  },
-  selectedStyle:{
-    backgroundColor: props.colorScheme == "dark" ? "#2F2F2F" : "#FDFBF7",
-    borderRadius: 12,
-    fontColor: props.colorScheme == "dark" ? "#FDFBF7" : "#2F2F2F",
-  }
-});
+const styles = (props: any) =>
+  StyleSheet.create({
+    dropdown: {
+      backgroundColor: props.colorScheme == "dark" ? "#2F2F2F" : "#FDFBF7",
+      fontColor: props.colorScheme == "dark" ? "white" : "#2F2F2F",
+      borderRadius: 10,
+      padding: 10,
+      width: 150,
+      marginRight: 10,
+    },
+    placeholderStyle: {
+      fontSize: 16,
+      color: props.colorScheme == "dark" ? "white" : "#2F2F2F",
+    },
+    selectedStyle: {
+      backgroundColor: props.colorScheme == "dark" ? "#2F2F2F" : "#FDFBF7",
+      borderRadius: 12,
+      fontColor: props.colorScheme == "dark" ? "#FDFBF7" : "#2F2F2F",
+    },
+  });
 export default function DropdownMenu(props: any) {
   const [selected, setSelected] = useState([]);
 
@@ -34,6 +35,7 @@ export default function DropdownMenu(props: any) {
       style={styles(props).dropdown}
       placeholderStyle={styles(props).placeholderStyle}
       selectedStyle={styles(props).selectedStyle}
+      activeColor="grey"
       search={true}
       labelField="label"
       valueField="value"
